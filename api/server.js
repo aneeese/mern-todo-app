1d9ac8d4d2bb4759de8bba89788e9c90be42577e
aneese421@gmail.com <aneese421@gmail.com>
2023-06-05 02:57:21 +0500

    cors update

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 
 // mount points
